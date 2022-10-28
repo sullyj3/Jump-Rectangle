bdddd2c8e244e43d87b348b5a6887cb095dab202
James Sully <sullyj3@gmail.com>
2022-10-29 08:27:57 +1100

    nix devshell with rust-analyzer

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  outputs = { self, nixpkgs }: 
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = [ pkgs.rust-analyzer ];
+    };
+
+  };
+}
